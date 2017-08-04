de556301e81f3350298ed9790526bb26a3e919b4
Reza Ali <rezaali@google.com>
2017-08-03 17:52:50 -0700

    added threejs example & cleaned up webgl example

--- WebARKit/WebARKit/ViewController.m
+++ WebARKit/WebARKit/ViewController.m
@@ -452,39 +452,42 @@ void extractQuaternionFromMatrix(const float *m, float *o) {
 - (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * _Nullable result))completionHandler
 {
     NSArray* values = [prompt componentsSeparatedByString:@":"];
+    if( [values count] > 1 ) {
     NSString* method = values[0];
     NSArray* params = [values[1] componentsSeparatedByString:@","];
     NSString* result = nil;
-    if ([method isEqualToString:@"hitTest"]) {
-        float x = [params[0] floatValue];
-        float y = [params[1] floatValue];
-        CGPoint point = CGPointMake(x, y);
-        ARFrame *currentFrame = [self.session currentFrame];
-        // TODO: Play with the different types of hit tests to see what corresponds best with what tango already provides.
-        NSArray<ARHitTestResult *> * hits = [currentFrame hitTest:point types:(ARHitTestResultType)ARHitTestResultTypeExistingPlaneUsingExtent];
-        //        NSArray<ARHitTestResult *> * hits = [currentFrame hitTest:point types:(ARHitTestResultType)ARHitTestResultTypeExistingPlane];
-        if (hits.count > 0)
-        {
-            matrix_float4x4 m = hits[0].worldTransform;
-            const float* matrix = (const float*)(&m);
-            float plane[4];
-            plane[0] = matrix[ 4];
-            plane[1] = matrix[ 5];
-            plane[2] = matrix[ 6];
-            plane[3] = 1;
-            float point[3];
-            point[0] = matrix[12];
-            point[1] = matrix[13];
-            point[2] = matrix[14];
-            result = [NSString stringWithFormat:@"{\"point\":[%f,%f,%f],\"plane\":[%f,%f,%f,%f]}", point[0], point[1], point[2], plane[0], plane[1], plane[2], plane[3]];
+        if ([method isEqualToString:@"hitTest"]) {
+            float x = [params[0] floatValue];
+            float y = [params[1] floatValue];
+            CGPoint point = CGPointMake(x, y);
+            ARFrame *currentFrame = [self.session currentFrame];
+            // TODO: Play with the different types of hit tests to see what corresponds best with what tango already provides.
+            NSArray<ARHitTestResult *> * hits = [currentFrame hitTest:point types:(ARHitTestResultType)ARHitTestResultTypeExistingPlaneUsingExtent];
+            //        NSArray<ARHitTestResult *> * hits = [currentFrame hitTest:point types:(ARHitTestResultType)ARHitTestResultTypeExistingPlane];
+            if (hits.count > 0)
+            {
+                matrix_float4x4 m = hits[0].worldTransform;
+                const float* matrix = (const float*)(&m);
+                float plane[4];
+                plane[0] = matrix[ 4];
+                plane[1] = matrix[ 5];
+                plane[2] = matrix[ 6];
+                plane[3] = 1;
+                float point[3];
+                point[0] = matrix[12];
+                point[1] = matrix[13];
+                point[2] = matrix[14];
+                result = [NSString stringWithFormat:@"{\"point\":[%f,%f,%f],\"plane\":[%f,%f,%f,%f]}", point[0], point[1], point[2], plane[0], plane[1], plane[2], plane[3]];
+            }
+            //        NSLog(@"WebARKit: hitTest hits count for (%f, %f) = %ld - %@", x, y, hits.count, data);
+            completionHandler(result);
         }
-        //        NSLog(@"WebARKit: hitTest hits count for (%f, %f) = %ld - %@", x, y, hits.count, data);
     }
     else
     {
         NSLog(@"%@", prompt);
+        completionHandler(@"");
     }
-    completionHandler(result);
 }
 
 #pragma mark - WKNavigationDelegate
